4e5af2284647a404b39da122c8b6bee5a3c2fc24
jueming <1579785608@qq.com>
2023-07-05 10:36:41 +0800

    公告栏、主题、杂项

--- ruoyi-ui/src/layout/components/Sidebar/Logo.vue
+++ ruoyi-ui/src/layout/components/Sidebar/Logo.vue
@@ -35,7 +35,7 @@ export default {
   },
   data() {
     return {
-      title: '若依管理系统',
+      title: '旅游综合信息平台',
       logo: logoImg
     }
   }
--- ruoyi-ui/src/views/login.vue
+++ ruoyi-ui/src/views/login.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="login">
     <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form">
-      <h3 class="title">若依后台管理系统</h3>
+      <h3 class="title">旅游综合信息平台</h3>
       <el-form-item prop="username">
         <el-input
           v-model="loginForm.username"
@@ -68,9 +68,9 @@
       </el-form-item>
     </el-form>
     <!--  底部  -->
-    <div class="el-login-footer">
-      <span>Copyright © 2018-2023 ruoyi.vip All Rights Reserved.</span>
-    </div>
+<!--    <div class="el-login-footer">-->
+<!--      <span>Copyright © 2018-2023 ruoyi.vip All Rights Reserved.</span>-->
+<!--    </div>-->
   </div>
 </template>
 
