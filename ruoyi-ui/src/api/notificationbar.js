1e0cc0346c9dd53a832176264b98db1d876b32cb
LryAkva <73147692+Lrynoa@users.noreply.github.com>
2023-07-07 16:31:30 +0800

    Update notificationbar.js

--- ruoyi-ui/src/api/notificationbar.js
+++ ruoyi-ui/src/api/notificationbar.js
@@ -16,30 +16,34 @@ export function getTodayTickets() {
   })
 }
 
-<<<<<<< Updated upstream
+
 // 查询当日事件
 export function getTodayEmergencies() {
   return request({
     url: '/system/emergencies/getTodayEmergencies',
-=======
-export function getPeopleCounts2() {
-  return request({
-    url: '/system/ticket_services/getPeopleCounts',
->>>>>>> Stashed changes
-    method: 'get',
   })
 }
 
-<<<<<<< Updated upstream
 // 查询当日投诉
 export function getTodayComplaints() {
   return request({
     url: '/system/complaints/getTodayComplaints',
-=======
+        method: 'get',
+  })
+}
+
+// 查询景区人数
+export function getPeopleCounts2() {
+  return request({
+    url: '/system/ticket_services/getPeopleCounts',
+    method: 'get',
+  })
+}
+
+//查询停车场车辆数
 export function getParkingCount2() {
   return request({
     url: '/system/cars/getParkingCount',
->>>>>>> Stashed changes
     method: 'get',
   })
 }
